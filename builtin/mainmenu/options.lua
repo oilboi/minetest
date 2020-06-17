6d3c52808d820e6a25b89b0c8a3782c9542dbb29
oilboi <47129783+oilboi@users.noreply.github.com>
2020-06-17 00:45:18 -0400

    Add in proper Fullscreen/unfullscreen setting

--- builtin/mainmenu/options.lua
+++ builtin/mainmenu/options.lua
@@ -1,4 +1,5 @@
 local fps = core.settings:get("fps_max")
+local fullscreen = core.settings:get("fullscreen")
 
 local function get_frame_rate()
     if fps == "15" then
@@ -9,11 +10,28 @@ local function get_frame_rate()
         return "button[5.5,8.5;5,2;button;FPS: "..fps.."]"
     end
 end
+
+--visual settings
+--[[
+ultra fast
+super fast
+fast
+nice
+super nice
+ultra nice
+amazing
+]]--
+
+local function get_fullscreen()
+    return "button[5.5,7;5,2;button;Fullscreen: "..fullscreen.."]"
+end
+
 --dofile(mainmenu_path .."minetest/init.lua")
 core.update_formspec(
     "size[16,12]"..
     "position[0.5,0.5]"..
-    --"bgcolor[#00000000]"..
+    "bgcolor[#00000000]"..
+    get_fullscreen()..
     get_frame_rate()..
     "button[5.5, 10 ;5,2;button;back]"
 )
@@ -49,11 +67,30 @@ function toggle_fps(button)
     core.settings:set("pause_fps_max",fps_choices[button_index])
 end
 
+function toggle_fullscreen(button)
+    button = string.gsub(button, "Fullscreen: ", "")
+    local screen = core.get_screen_info()
+    if button == "true" then
+        button = "false"
+        core.settings:set("screen_w",screen.display_width/2)
+        core.settings:set("screen_h",screen.display_height/2)
+    elseif button == "false" then
+        core.settings:set("screen_w",screen.display_width)
+        core.settings:set("screen_h",screen.display_height)
+        button = "true"
+    end
+
+    core.settings:set("fullscreen", button)
+end
+
 core.button_handler = function(event)
     if event.button then
         core.sound_play("button", false)
         if string.match(event.button,"FPS: ") then
             toggle_fps(event.button)
+            dofile(mainmenu_path .. "options.lua")
+        elseif string.match(event.button,"Fullscreen: ") then
+            toggle_fullscreen(event.button)
             dofile(mainmenu_path .. "options.lua")
         elseif event.button == "back" then
             dofile(mainmenu_path .. "init.lua")
