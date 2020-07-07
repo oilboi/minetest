2d646402c7699954fe305a753e3da3d50b95a190
oilboi <47129783+oilboi@users.noreply.github.com>
2020-07-07 12:33:25 -0400

    Update config.h

--- src/config.h
+++ src/config.h
@@ -11,11 +11,6 @@
 
 #if defined USE_CMAKE_CONFIG_H
 	#include "cmake_config.h"
-#elif defined (__ANDROID__)
-	#define PROJECT_NAME "minetest"
-	#define PROJECT_NAME_C "Minetest"
-	#define STATIC_SHAREDIR ""
-	#define VERSION_STRING STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR(VERSION_PATCH) STR(VERSION_EXTRA)
 #ifdef NDEBUG
 		#define BUILD_TYPE "Release"
 	#else
